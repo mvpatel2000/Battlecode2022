95828dd5c783ada41386db6e64017330ddd582ca
Mihir <mihir.v.patel7@gmail.com>
2022-01-04 22:44:06 -0800

    add miner kiting

--- src/hermes/Miner.java
+++ src/hermes/Miner.java
@@ -4,11 +4,16 @@ import battlecode.common.*;
 
 public class Miner extends Robot {
 
+    int fleeingCounter;
+    MapLocation lastEnemyLocation;
+
     final static int[][] INNER_SPIRAL_ORDER = {{0,0},{0,1},{1,0},{0,-1},{-1,0},{1,1},{1,-1},{-1,-1},{-1,1},{0,2},{2,0},{0,-2},{-2,0},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1},{-2,1},{-1,2},{2,2},{2,-2},{-2,-2},{-2,2},{0,3},{3,0},{0,-3},{-3,0},{1,3},{3,1},{3,-1},{1,-3},{-1,-3},{-3,-1},{-3,1},{-1,3},{2,3},{3,2},{3,-2},{2,-3},{-2,-3},{-3,-2},{-3,2},{-2,3}};
     final static int[][] OUTER_SPIRAL_ORDER = {{0,4},{4,0},{0,-4},{-4,0},{1,4},{4,1},{4,-1},{1,-4},{-1,-4},{-4,-1},{-4,1},{-1,4},{3,3},{3,-3},{-3,-3},{-3,3},{2,4},{4,2},{4,-2},{2,-4},{-2,-4},{-4,-2},{-4,2},{-2,4}};
     
     public Miner(RobotController rc) throws GameActionException {
         super(rc);
+        fleeingCounter = 0;
+        lastEnemyLocation = null;
     }
 
     @Override
@@ -53,8 +58,38 @@ public class Miner extends Robot {
 
     public void move() throws GameActionException {
         updateDestination();
-        rc.setIndicatorLine(myLocation, destination, 255, 0, 0);
-        fuzzyMove(destination);
+        
+        // Find nearest combat enemy to kite
+        RobotInfo[] enemies = rc.senseNearbyRobots(RobotType.MINER.visionRadiusSquared, enemyTeam);
+        MapLocation nearestCombatEnemy = null;
+        int distanceToEnemy = Integer.MAX_VALUE;
+        for (RobotInfo enemy : enemies) {
+            if (enemy.getType() == RobotType.SOLDIER || enemy.getType() == RobotType.SAGE 
+                    || enemy.getType() == RobotType.WATCHTOWER) {
+                int dist = myLocation.distanceSquaredTo(enemy.location);
+                if (dist < distanceToEnemy) {
+                    nearestCombatEnemy = enemy.location;
+                    distanceToEnemy = dist;
+                }
+            }
+        }
+        if (nearestCombatEnemy != null) { 
+            lastEnemyLocation = nearestCombatEnemy;
+            fleeingCounter = 5;
+        }
+
+        // Kite enemy unit
+        if (fleeingCounter > 0) {
+            MapLocation fleeDirection = myLocation.add(myLocation.directionTo(lastEnemyLocation).opposite());
+            fuzzyMove(fleeDirection);
+            rc.setIndicatorLine(myLocation, fleeDirection, 255, 0, 0);
+            fleeingCounter--;
+        }
+        // Path
+        else {
+            fuzzyMove(destination);
+            rc.setIndicatorLine(myLocation, destination, 255, 0, 0);
+        }
     }
 
     /**
