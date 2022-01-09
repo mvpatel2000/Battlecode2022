5ffcefc68283fd48a5e1452f7cec6962475d8c57
Vinjai Vale <vinjai.vale@gmail.com>
2022-01-08 23:47:53 -0800

    Comms API for reading/writing archon data and cluster data

--- src/hermes/Archon.java
+++ src/hermes/Archon.java
@@ -4,15 +4,30 @@ import battlecode.common.*;
 
 public class Archon extends Robot {
 
+    int myArchonNum;
+    int numOurArchons;
+
     public Archon(RobotController rc) throws GameActionException {
         super(rc);
     }
 
     @Override
     public void runUnit() throws GameActionException {
-        // if (currentRound > 34) {
-        //     rc.disintegrate();
-        // }
+        switch (currentRound) {
+            case 1:
+                firstRound();
+                break;
+            case 2:
+                secondRound();
+                break;
+            default:
+                rc.resign();
+                mainLoop();
+                break;
+        }
+    }
+
+    public void mainLoop() throws GameActionException {
         boolean shouldBuildMiner = turnCount < 100 ? rng.nextBoolean() : rng.nextDouble() < 0.3;
         RobotType toBuild = shouldBuildMiner ? RobotType.MINER : RobotType.SOLDIER;
         // Build builders if lots of lead for watchtowers
@@ -57,5 +72,41 @@ public class Archon extends Robot {
                 rc.repair(optimalRepair);
             }
         }
+    }
+
+    /**
+     * First round, find out our archon number and set our status and location
+     * 
+     * @throws GameActionException
+     */
+    public void firstRound() throws GameActionException {
+        myArchonNum = 0;
+        if (commsHandler.readOurArchonStatus(0) == 1) {
+            myArchonNum = 1;
+        } if (commsHandler.readOurArchonStatus(1) == 1) {
+            myArchonNum = 2;
+        } if (commsHandler.readOurArchonStatus(2) == 1) {
+            myArchonNum = 3;
+        }
+        System.out.println("I am archon number " + myArchonNum);
+        commsHandler.writeOurArchonStatus(myArchonNum, 1);
+        commsHandler.writeOurArchonLocation(myArchonNum, myLocation);
+    }
+
+    /**
+     * Second round, find out how many archons we have
+     * 
+     * @throws GameActionException
+     */
+    public void secondRound() throws GameActionException {
+        numOurArchons = 1;
+        if (commsHandler.readOurArchonStatus(1) == 1) {
+            numOurArchons++;
+        } if (commsHandler.readOurArchonStatus(2) == 1) {
+            numOurArchons++;
+        } if (commsHandler.readOurArchonStatus(3) == 1) {
+            numOurArchons++;
+        }
+        System.out.println("Our team has " + numOurArchons + " archons");
     }
 }
--- test/hermes/CommsTest.java
+++ test/hermes/CommsTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import battlecode.common.GameActionException;
-import hermes.CommsHandler;
 
 public class CommsTest {
 
