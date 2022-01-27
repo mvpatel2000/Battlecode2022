9f6ceec1fcb1e586089b3d80eb8c82a150e3c816
Mihir <mihir.v.patel7@gmail.com>
2022-01-27 13:43:07 -0800

    comment out dead code

--- src/athena/Robot.java
+++ src/athena/Robot.java
@@ -849,7 +849,7 @@ public class Robot {
         boolean isNotSageOrIsActionReady = rc.getType() != RobotType.SAGE || rc.isActionReady();
         boolean atMaxHealth = rc.getHealth() == rc.getType().getMaxHealth(rc.getLevel());
 
-        double combatAllyHealth = 0.0;
+        // double combatAllyHealth = 0.0;
         int allyCount = 0;
         boolean isArchonVisible = false;
         // MapLocation archonLocation = null;
@@ -859,7 +859,7 @@ public class Robot {
         for (int i = 0; i < alliesLength; i++) {
             RobotInfo ally = allies[i];
             if (ally.type == RobotType.WATCHTOWER || (ally.type == RobotType.SOLDIER && ally.health > FLEE_HEALTH)) { // && ally.health > FLEE_HEALTH
-                combatAllyHealth += ally.health;
+                // combatAllyHealth += ally.health;
                 if (myLocation.distanceSquaredTo(ally.location) <= 5) {
                     allyCount++;
                 }
